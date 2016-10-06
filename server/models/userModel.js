af33afd2617badef3296b2821701674be40012fe
A-Reum Jung <areumjung1987@gmail.com>
2016-10-06 17:34:03 -0600

    edited user schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,8 +5,22 @@ var userSchema = new mongoose.Schema({
     firstName: {type: String},
     lastName: {type: String}
   },
+  username: {type: String, required: true, unique:true},
   email: {type: String, required: true, unique: true},
-  password: {type: String, required: true}
+  password: {type: String, required: true},
+  profile: {
+    description: {type: String},
+    gender: {type: String},
+    teamSport: {type: String},
+    teamName: {type: String},
+    numberOfTeamMembers: {type: Number},
+    teamMembers: {
+      name: {type: String},
+      position: {type: String}
+    },
+    location: {type: String, required: true}
+  }
+
 });
 
 module.exports = mongoose.model("User", userSchema);
